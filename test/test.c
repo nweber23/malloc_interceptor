641b0a7971b960e3d9b70a8932a3727d69385741
Niklas Weber <nweber@student.42Heilbronn.de>
2025-10-17 18:34:06 +0200

    new tester

--- test/test.c
+++ test/test.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 int main(void) {
+    char buf[64];
     for (int i = 1; i <= 50; ++i) {
         void *p = malloc(10);
-        if (!p) {
-            printf("malloc #%d failed!\n", i);
-        }
-		else {
-			printf("malloc #%d succeeded\n", i);
-			free(p);
-		}
+        int len = snprintf(buf, sizeof(buf), "malloc #%d %s\n", i, p ? "succeeded" : "failed!");
+        write(1, buf, len);
+        if (p) free(p);
     }
     return 0;
 }
